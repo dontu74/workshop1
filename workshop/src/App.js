3344d16debcb061b70a019d44efccf72cc859a92
unknown <dongtu@gmail.com>
2020-03-25 21:31:37 -0400

    added event and form

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor () {
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    }
+  }
+addItem = (event) => {
+  event.preventDefault();
+  console.log("The addItem method executed");
+
 }
 
-export default App;
+handleChange = (event) => {
+  console.log(event.target.value);
+}
+
+  render() {
+    return (
+      <div className='contianer'>
+        <form onSubmit={this.addItem}>
+        <label htmlFor="taskName">Task Name:</label>
+        <input onChange={this.handleChange}name="taskName"type="text"placeholder="Add todo here!" />
+        <button type="submit">Add Task</button>
+        </form>
+    </div>
+  )
+  }
+}
+
+
+export default ToDoList;
